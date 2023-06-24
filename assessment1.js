d8f7dbefe0ced4b8e955ad8ca20a8666a435974e
sangam2256 <132322216+sangam2256@users.noreply.github.com>
2023-06-24 21:20:03 +0530

    Create assessment1.js

---
+++ assessment1.js
@@ -0,0 +1,51 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const NFTs=[];
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (_name,_eyecolor,_shirt,_bling) {                     //_(underscore) is used to distinguish between parametrs and properties of objects
+const NFT={
+ "name":_name,
+ "eye_color":_eyecolor,
+ "shirt":_shirt,
+ "bling":_bling
+}
+NFTs.push(NFT);
+console.log("Minted: "+_name)
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+for(let i=0;i<NFTs.length;i++){
+    console.log("\nID:\t\t"+(i+1))                                       // '\t' is just used to add space for proper alignment
+    console.log("Name: \t\t"+NFTs[i].name)
+    console.log("Eye Color: \t"+NFTs[i].eye_color)
+    console.log("Shirt: \t\t"+NFTs[i].shirt)
+    console.log("Bling: \t\t"+NFTs[i].bling)
+}
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+  console.log("\n"+NFTs.length)
+}
+
+// call your functions below this line
+mintNFT("Sangam","Black","Formal","Gold Chain");
+mintNFT("Tushar","Blue","T-Shirt","Silver Chain");
+mintNFT("Priya","Brown","Hoodie","Platinum Chain");
+mintNFT("Shiv","Grey","Kurta","Diamond");
+listNFTs()
+getTotalSupply()
